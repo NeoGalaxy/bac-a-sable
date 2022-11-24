817ab22f2f771db823768a2615124fb42d87ec22
Syméon Smith <symeon.smith@gmail.com>
2022-11-24 17:47:16 +0100

    Remove SvelteKit project

--- client/src/lib/stores.ts
+++
@@ -1,8 +0,0 @@
-import { writable, type Writable } from "svelte/store"
-import type { Filters } from "./types"
-
-export const filters: Writable<Filters> = writable({
-	biasCorrection: false,
-	comparison: 0,
-	methods: [0, 1],
-})
--- client/src/lib/types.ts
+++
@@ -1,5 +0,0 @@
-export type Filters = {
-	biasCorrection: boolean
-	comparison: number
-	methods: number[]
-}
