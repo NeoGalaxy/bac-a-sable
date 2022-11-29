e3bd7709fa764cf0fc1db496ab1296916dd07964
Habibata Samake <habibata.samake@grenoble-inp.org>
2022-11-29 18:42:03 +0100

    rapport

--- client-svelte/src/pages/ReportPage.svelte
+++ client-svelte/src/pages/ReportPage.svelte
@@ -11,13 +11,38 @@
 		<li>Syméon SMITH</li>
 	</ul>
 
-	<h2>Introduction</h2>
-
+	<h2>Introduction </h2>
+	<div><p>
+		Le but de ce projet Challenge Open Data est de produire un visuel sur de différentes méthodes de votes réalisés en ligne offrant ainsi l'occasion de comprendre les opinions des participants sur les candidats à la presidentielle de 2017 et faire un lien avec les votes officiels. 
+	</p></div>
 	<h2>Jeu de données et représentation sur l'application</h2>
+	<div>
+		<p>
+			cf pre-rapport
+			Le jeu de données sélectionné pour ce challenge est issu d’une expérimentation en ligne sur les votes des élections présidentielles de 2017 en France.
+			Près de 37739 personnes ont participé à l’expérience. Il leur a été demandé de remplir des bulletins de vote représentant différentes méthodes de vote. Les participants ont également répondu à différents questionnaires concernant par exemple leur jugement de la pertinence d’une telle démarche ou leur niveau de satisfaction quant à leur vote réellement exprimé au premier tour des élections présidentielles.
 
+			Bien que nous disposions aussi d’indicateurs socio-professionnels (sexe, tranche d’âge, niveau d’éducation, etc), nous avons décidé de nous concentrer uniquement sur les données exprimées dans les bulletins de vote sans considérer ces autres facteurs. En effet, nous avons pour objectif de réaliser des visualisations de données permettant de comparer et de mettre en opposition les différentes méthodes de vote et leur influence sur le classement final des candidats.
+
+			Afin de pouvoir réaliser cela, nous devons dans un premier temps traiter les données afin de les homogénéiser pour pouvoir les rendre exploitables. A cet effet, après pre-traitement de nos données, nous ne retenons que les resultats finaux pour les méthodes, les scores finaux pour les candidats qu'ont obtenu les candidats selons les méthodes de vote.
+		</p>
+	</div>
 	<h2>Architecture de l'application</h2>
+	<div>
+		<p>Dans notre architecture, nous avons principalement deux pages: l'une "ReportPage.svelte" où se trouve le rapport et l'autre  "GraphPage" où sont faites les différentes repesentation graphiques.Ces deux pages sont récuperés dans le fichier index.html qui se trouve à la racine du projet. Nous avons une architecture arborescente contenant les dossiers suivants: 
+		</p>
+		<ul>
+			<li><strong>src</strong>: contient tout le code source su projet, incluant les feuilles de styles, les librairies et les différents composants de l'application, mais aussi nos datasets contenant les infrormations nécessaires pour la représentation graphique pour les différentes méthodes.
+			</li>
 
+		</ul>
+	</div>
+	
+	
 	<h3>Technologies utilisées</h3>
+	<div>
+
+
 
 	<ul>
 		<li>
