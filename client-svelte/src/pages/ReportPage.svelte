91c400c35cdf26be646f7bd8b2d51db53e57afee
Syméon Smith <symeon.smith@gmail.com>
2022-11-30 10:25:07 +0100

    Report tweaks

--- client-svelte/src/pages/ReportPage.svelte
+++ client-svelte/src/pages/ReportPage.svelte
@@ -11,38 +11,69 @@
 		<li>Syméon SMITH</li>
 	</ul>
 
-	<h2>Introduction </h2>
-	<div><p>
-		Le but de ce projet Challenge Open Data est de produire un visuel sur de différentes méthodes de votes réalisés en ligne offrant ainsi l'occasion de comprendre les opinions des participants sur les candidats à la presidentielle de 2017 et faire un lien avec les votes officiels. 
-	</p></div>
+	<h2>Introduction</h2>
+
+	<p>
+		Le but de ce Challenge Open Data est de produire un visuel sur différentes
+		méthodes de votes grâce à une expérience réalisée en ligne, offrant ainsi
+		l'occasion de comprendre les opinions des participants sur les candidats à
+		la présidentielle de 2017 et de faire un lien avec les résultats du vote
+		officiel.
+	</p>
+
 	<h2>Jeu de données et représentation sur l'application</h2>
-	<div>
-		<p>
-			cf pre-rapport
-			Le jeu de données sélectionné pour ce challenge est issu d’une expérimentation en ligne sur les votes des élections présidentielles de 2017 en France.
-			Près de 37739 personnes ont participé à l’expérience. Il leur a été demandé de remplir des bulletins de vote représentant différentes méthodes de vote. Les participants ont également répondu à différents questionnaires concernant par exemple leur jugement de la pertinence d’une telle démarche ou leur niveau de satisfaction quant à leur vote réellement exprimé au premier tour des élections présidentielles.
 
-			Bien que nous disposions aussi d’indicateurs socio-professionnels (sexe, tranche d’âge, niveau d’éducation, etc), nous avons décidé de nous concentrer uniquement sur les données exprimées dans les bulletins de vote sans considérer ces autres facteurs. En effet, nous avons pour objectif de réaliser des visualisations de données permettant de comparer et de mettre en opposition les différentes méthodes de vote et leur influence sur le classement final des candidats.
+	<p>
+		<!-- cf pre-rapport --> Le jeu de données sélectionné pour ce challenge est issu
+		d’une expérimentation en ligne sur les votes des élections présidentielles de
+		2017 en France. Près de 37739 personnes ont participé à l’expérience. Il leur
+		a été demandé de remplir des bulletins de vote représentant différentes méthodes
+		de vote. Les participants ont également répondu à différents questionnaires concernant
+		par exemple leur jugement de la pertinence d’une telle démarche ou leur niveau
+		de satisfaction quant à leur vote réellement exprimé au premier tour des élections
+		présidentielles. Bien que nous disposions aussi d’indicateurs socio-professionnels
+		(sexe, tranche d’âge, niveau d’éducation, etc), nous avons décidé de nous concentrer
+		uniquement sur les données exprimées dans les bulletins de vote sans considérer
+		ces autres facteurs.
+	</p>
+	<p>
+		En effet, nous avons pour objectif de réaliser des visualisations de données
+		permettant de comparer et de mettre en opposition les différentes méthodes
+		de vote et leur influence sur le classement final des candidats. Afin de
+		pouvoir réaliser cela, nous devons dans un premier temps traiter les données
+		afin de les homogénéiser pour pouvoir les rendre exploitables. À cet effet,
+		après pré-traitement de nos données, nous ne retenons que les scores finaux
+		qu'ont obtenu les candidats selons les méthodes de vote.
+	</p>
 
-			Afin de pouvoir réaliser cela, nous devons dans un premier temps traiter les données afin de les homogénéiser pour pouvoir les rendre exploitables. A cet effet, après pre-traitement de nos données, nous ne retenons que les resultats finaux pour les méthodes, les scores finaux pour les candidats qu'ont obtenu les candidats selons les méthodes de vote.
-		</p>
-	</div>
 	<h2>Architecture de l'application</h2>
-	<div>
-		<p>Dans notre architecture, nous avons principalement deux pages: l'une "ReportPage.svelte" où se trouve le rapport et l'autre  "GraphPage" où sont faites les différentes repesentation graphiques.Ces deux pages sont récuperés dans le fichier index.html qui se trouve à la racine du projet. Nous avons une architecture arborescente contenant les dossiers suivants: 
-		</p>
-		<ul>
-			<li><strong>src</strong>: contient tout le code source su projet, incluant les feuilles de styles, les librairies et les différents composants de l'application, mais aussi nos datasets contenant les infrormations nécessaires pour la représentation graphique pour les différentes méthodes.
-			</li>
 
-		</ul>
-	</div>
-	
-	
+	<p>
+		L’application est structurée en deux couches&nbsp;: une couche de
+		pré-traitement des données, permettant de mettre en forme les données et de
+		les rendre plus digestes, et une couche applicative qui reprend ces données,
+		applique si besoin des traitements plus légers, et les affiche sous forme de
+		graphes interactifs.
+	</p>
+	<p>
+		La partie de pré-traitement des données consiste en un script qui récupère
+		pour chaque scrutin les résultats finaux des candidats et les rassemble en
+		un jeu de données au format bien défini. Ces données sont ensuite insérées
+		dans l’application dans une variable JavaScript directement, pour diminuer
+		les temps de traitements éventuels qui pourraient être liés à la
+		récupération depuis un fichier ou un serveur.
+	</p>
+	<p>
+		L’application web utilise cette variable pour afficher le
+		«&nbsp;Bi-graphe&nbsp;» car celui-ci nécessite de connaître les résultats
+		par méthode de vote. Par contre, le «&nbsp;Multi-graphe&nbsp;» a besoin
+		d’avoir les données mises en forme par candidat (puisqu’il affiche les
+		résultats de chaque méthode sélectionnée, pour chaque candidat). Cette mise
+		en forme est réalisée au lancement de l’application et stockée dans une
+		autre variable.
+	</p>
+
 	<h3>Technologies utilisées</h3>
-	<div>
-
-
 
 	<ul>
 		<li>
@@ -91,11 +122,13 @@
 
 <style>
 	main {
-		max-width: 65ch;
+		max-width: 68ch;
 		margin: auto;
 		line-height: 1.3;
 		margin-top: 3rem;
 		margin-bottom: 8rem;
+		text-align: justify;
+		padding: 0 1rem;
 	}
 
 	h1,
